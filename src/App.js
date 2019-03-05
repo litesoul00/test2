2f481c5fb44d52eb0839df9bec4d650397919af0
litesoul00 <jasons8002@gmail.com>
2019-03-05 00:51:28 -0800

    added delete and add function

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
 import React, { Component } from 'react';
+import Note from '../src/components/note';
 
 export default class App extends Component {
     state = {
         noteText: '',
-        note: [],
+        notes: [],
     }
 
-    updateNoteText(noteText) {
-        this.setState({noteText: noteText.target.value})
+    updateNoteText( noteText ) {
+        this.setState({ noteText : noteText.target.value })
+        console.log( this.state.noteText )
     }
-    
-    addNote = (event) => {
-        event.preventDefault()
-        if (this.state.noteText === '')
-            {return false}
+
+    addNote() {
+        if ( this.state.noteText === '' ) 
+        { return false }
         let notesArr = this.state.notes;
-        notesArr.push(this.state.noteText);
-        this.setState({noteText:''});
+        notesArr.push( this.state.noteText );
+        this.setState({ noteText : '' })
         this.textInput.focus();
-        console.log('add')
     }
 
-    render() {
-        return (
-            <form onSubmit = {this.addNote}>
-                <input 
-                    type = "text"
-                    placeholder = "add task"
-                    onChange = {noteText => this.updateNoteText(noteText)}
-                />
-                <input 
-                    type = "submit" 
-                    value = "Add to list"
-                />
-            </form>
-        );
+    deleteNote( index ) {
+        let notesArr = this.state.notes;
+        notesArr.splice( index, 1 );
+        this.setState({ notes : notesArr })
     }
-}
+
+        render() {
+
+            let notes = this.state.notes.map (( val,key ) => {
+                return <Note 
+                        key = {key} 
+                        text = {val}
+                        deleteMethod = {() => this.deleteNote( key )}/>
+            })
+            return (
+                <div className = "container">
+                
+                    <div className = "header"><h1>React Todo Test</h1></div>
+                    
+                    {notes}
+                    
+                    <div className = "btn"
+                         onClick = { this.addNote.bind( this ) }><h2>+</h2></div>
+
+                    <input type = "text"
+                        ref = {(( input ) => { this.textInput = input })}
+                        className = "textInput"
+                        value = { this.state.noteText }
+                        onChange = { noteText => this.updateNoteText(noteText) }
+                    />
+                </div>
+            )
+        }
+    }
